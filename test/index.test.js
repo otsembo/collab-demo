820e94d1528404aa2d7951536f9f0fb05803053b
Ian Otsembo <38042565+otsembo@users.noreply.github.com>
2022-05-19 15:56:30 +0300

    Added division test

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,11 @@
 const assert = require('assert')
-const { add } = require('../index.js')
+const { add, divide } = require('../index.js')
 
 describe('Math tests', () => {
     it('Addition', () => {
-        
         assert.equal(add(1,2), 3);
+    })
+    it('Divide', () => {
+        assert.equal(divide(2,1), 2);
     })
 })
