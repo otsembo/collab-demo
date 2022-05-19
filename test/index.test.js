fd6f55fb595b9535be554429e22bdba599eb3658
Ian Otsembo <38042565+otsembo@users.noreply.github.com>
2022-05-19 16:02:50 +0300

    Added multiplication

--- test/index.test.js
+++ test/index.test.js
@@ -6,6 +6,9 @@ describe('Math tests', () => {
         assert.equal(add(1,2), 3);
     })
     it('Divide', () => {
-        assert.equal(divide(2,1), 1);
+        assert.equal(divide(2,2), 1);
+    })
+    it('Multiplication Test', () => {
+        assert.equal(divide(2,3), 1);
     })
 })
